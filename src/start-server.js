88afefac4cb5d913f8e36ac917690f4063a3c89b
Nick Fraenkel <nickfraenkel@gmail.com>
2025-04-20 21:46:51 +0100

    Parse SELECT statements to AST
    I've cheated a little bit here: I'm using "," to explode the list into
    items rather than consume it token by token. As a result we're not
    really checking for, eg. a proper terminator.
    
    Still, this will probably do for the purposes of creating a simple AST
    that we can then convert into JSON and return.

--- src/start-server.js
+++ src/start-server.js
@@ -1,5 +1,43 @@
 import net from "node:net";
 
+function node(type, value) {
+  return { type, value };
+}
+
+function parseLiteral(statement) {
+  if (statement.startsWith(";") || statement.startsWith("\x00")) {
+    return [];
+  }
+
+  if (statement.startsWith("TRUE")) {
+    return node("BOOLEAN", true);
+  }
+
+  if (statement.startsWith("FALSE")) {
+    return node("BOOLEAN", false);
+  }
+
+  let n;
+
+  if ((n = statement.match(/^-?\d+/))) {
+    return node("INTEGER", Number(n));
+  }
+}
+
+function parseList(statement) {
+  return statement
+    .split(",")
+    .map((literal) => parseLiteral(literal.trimStart()));
+}
+
+function parse(statement) {
+  if (statement.startsWith("SELECT")) {
+    return node("SELECT", parseList(statement.slice(7)));
+  }
+
+  throw new Error("Unexpected statement");
+}
+
 const server = net.createServer((socket) => {
   let message = "";
 
@@ -7,6 +45,9 @@ const server = net.createServer((socket) => {
     message += data;
 
     if (message.endsWith("\0")) {
+      const ast = parse(message);
+      console.log(ast);
+
       message = "";
       socket.write("Hello\0");
     }
